702842ac52c515e35576d566868007b5026ff1ff
Kelly Stoops <kstoops815@gmail.com>
2018-06-02 11:13:36 -0500

    teachers, students, and locations models setup

---
+++ BackendCapstone/Models/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Models
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        public bool IsAtSchool { get; set; }
+        public int HomeroomTeacherId { get; set; }
+        public bool InTransit { get; set; }
+        public bool IsGoneTooLong { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
