1feff732a0753ff4f1b70a432ed42b6da085751d
Kelly Stoops <kstoops815@gmail.com>
2018-06-02 12:54:13 -0500

    studentLocations model setup

---
+++ BackendCapstone/Models/StudentLocationsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Models
+{
+    public class StudentLocationsModel
+    {
+        public int StudentLocationsId { get; set; }
+        public int StudentId { get; set; }
+        public int LocationId { get; set; }
+        public DateTime CheckedIn { get; set; }
+        public DateTime CheckedOut { get; set; }
+
+    }
+}
