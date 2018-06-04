41e186ce4b51d784af2d54c92d9f29000519845e
Kelly Stoops <kstoops815@gmail.com>
2018-06-03 19:53:39 -0500

    completed models setup

---
+++ BackendCapstone/Controllers/TeachersController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Controllers
+{
+    public class TeachersController
+    {
+    }
+}
