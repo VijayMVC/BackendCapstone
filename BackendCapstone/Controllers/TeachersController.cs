4f0732ca17dff012f2c8957dfcafc02cefced548
Kelly Stoops <kstoops815@gmail.com>
2018-06-04 19:33:42 -0500

    read query on teachers working

--- BackendCapstone/Controllers/TeachersController.cs
+++ BackendCapstone/Controllers/TeachersController.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using BackendCapstone.Models;
+using BackendCapstone.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 
 namespace BackendCapstone.Controllers
 {
-    public class TeachersController
+    [RoutePrefix("api/teachers")]
+    public class TeachersController : ApiController
     {
+        [HttpGet, Route("")]
+        public HttpResponseMessage DisplayTeachers(TeacherModel teacher)
+        {
+            var teacherInfo = new TeachersRepository();
+            var displayTeachers = teacherInfo.GetTeachers();
 
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, displayTeachers);
+        }
     }
+
 }
