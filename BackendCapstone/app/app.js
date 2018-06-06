0c933b1ec74e62aa96ccd050a7796a4a86448bdf
Kelly Stoops <kstoops815@gmail.com>
2018-06-05 19:17:36 -0500

    initial routing setup

--- BackendCapstone/app/app.js
+++ BackendCapstone/app/app.js
@@ -1,1 +1,25 @@
-﻿
+﻿var app = angular.module("BackendCapstone", ["ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+        .when("/",
+        {
+            templateUrl: "/app/partials/index.html",
+            controller: "HomeController"
+        })
+        .when("/teachers",
+        {
+            templateUrl: "/app/partials/teachers.html",
+            controller: "TeachersController"
+        })
+        .when("/locations",
+        {
+            templateUrl: "/app/partials/locations.html",
+            controller: "LocationsController"
+        })
+        .when("/students",
+        {
+            templateUrl: "/app/partials/students.html",
+            controller: "StudentsController"
+        });
+}]);
