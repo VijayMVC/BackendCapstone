f219c16d0752b70149cee5aa424b9d870d70654d
Kelly Stoops <kstoops815@gmail.com>
2018-06-11 19:51:22 -0500

    select homeroom teacher working

--- BackendCapstone/app/controllers/StudentViewController.js
+++ BackendCapstone/app/controllers/StudentViewController.js
@@ -1,10 +1,19 @@
 ﻿app.controller("StudentViewController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
 
+    $scope.homeroomTeacher = {};
+    
 
     $http.get("/api/teachers").then(function (result) {
         $scope.homeroomTeachers = result.data;
+        console.log($scope.homeroomTeachers);
     });
 
+    $scope.selectTeacher = () => {
+        console.log("i selected a teacher");
+        $http.get(`/api/teachers/${$scope.homeroomTeacher.TeacherId}`).then(function (result) {
+            console.log(result.data);
+        });
+    };
 
 
 }]);
